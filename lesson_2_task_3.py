062d341260863f1ace25040a7f47fd42d71b6eaf
MikhailKuznetcov-135-2 <darkwolfpzd@gmail.com>
2026-07-10 14:05:07 +0300

    Delete lesson_2_task_3.py

--- lesson_2_task_3.py
+++
@@ -1,9 +0,0 @@
-import math
-
-def square(side) -> float:
-    area = side * side
-    # Если результат не целый, округляем вверх
-    if area != int(area):
-        return math.ceil(area)
-    return area
-
